19cf89a76d5935fad37293a29a188daaa4adaecd
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-07-03 14:26:37 +0200

    Update Tanstack to v124

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    // tailwindcss(), sentry(), ...
+    tanstackStart({ /** Add your options here */ })
+  ]
+})
